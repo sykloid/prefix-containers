820dc711b1da244cc0f3e0030f01456f2e76421d
P.C. Shyamshankar <sykora@lucentbeing.com>
2013-02-28 19:10:45 -0500

    Add insertion functions for Tries.
    I've tried to follow the convention for data structure APIs set by
    packages such as containers, and that seems to be working so far.

--- src/Data/Trie/Map.hs
+++ src/Data/Trie/Map.hs
@@ -1,7 +1,16 @@
 -- | An associative map implemented as a prefix-tree, supports efficient lookup by prefix.
 module Data.Trie.Map (
     TrieMap(..),
+
+    -- * Primitive Tries
+    empty,
+
+    -- * Trie Modification
+    insert,
+    insertWith,
 ) where
+
+import Data.Maybe
 
 -- | The basic Trie data structure. Each node contains the value associated with the current prefix,
 -- and a set of sub-tries based on the symbols that can appear subsequently.
@@ -19,3 +28,20 @@ data TrieMap k v
 -- sequence
 empty :: TrieMap k v
 empty = TrieMap Nothing []
+
+-- | Insert a sequence and a value into the Trie. If the sequence is already present in the Trie,
+-- apply the function to the new and old values, and insert the result into the Trie.
+insertWith :: Eq k => (v -> v -> v) -> [k] -> v -> TrieMap k v -> TrieMap k v
+insertWith f [] v t = t { label = Just $ maybe v (f v) (label t) }
+insertWith f ks v t = t { submaps = submapInsertWith f ks v (submaps t) }
+  where
+    submapInsertWith :: Eq k => (v -> v -> v) -> [k] -> v -> [(k, TrieMap k v)] -> [(k, TrieMap k v)]
+    submapInsertWith f (x:xs) v [] = [(x, insertWith f xs v empty)]
+    submapInsertWith f (x:xs) v ((c, m):ms)
+        | x == c = (c, insertWith f xs v m) : ms
+        | otherwise = (c, m) : submapInsertWith f (x:xs) v ms
+
+-- | Insert a sequence and a value into the Trie, overriding any value which might have been there
+-- already for that sequence.
+insert :: Eq k => [k] -> v -> TrieMap k v -> TrieMap k v
+insert = insertWith const
